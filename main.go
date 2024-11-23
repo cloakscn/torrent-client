fec31f34df1ba3c27b41299498bb64145fa334d1
root <root@gxyhkpro2ecs3PByr6WgP111.local>
2024-11-23 10:06:53 +0000

    feat: 完成客户端拉取基本流程

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/cloakscn/bit-torrent-client/torrentfile"
+)
 
 func main() {
 	fmt.Println("Hello bit-torrent-client!")
+
+	inPath := os.Args[1]
+	outPath := os.Args[2]
+
+	tf, err := torrentfile.Open(inPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tf.DownloadToFile(outPath)
+	if err != nil {
+		log.Fatal(err)
+	}	
 }
